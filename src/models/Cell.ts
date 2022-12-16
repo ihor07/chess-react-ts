6678954b3cb261e062c3d6ed3b29003bc86683e1
Ihor <ngorekk@gmail.com>
2022-12-16 23:52:28 +0100

    board finish

---
+++ src/models/Cell.ts
@@ -0,0 +1,29 @@
+import { Board } from './Board'
+import { Colors } from './Colors'
+import { Figure } from './figures/Figure'
+
+export class Cell {
+  readonly x: number
+  readonly y: number
+  readonly color: Colors
+  figure: Figure | null
+  board: Board
+  avaliable: boolean
+  id: number //for key react
+
+  constructor(
+    board: Board,
+    x: number,
+    y: number,
+    color: Colors,
+    figure: Figure | null
+  ) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.figure = figure
+    this.board = board
+    this.avaliable = false
+    this.id = Math.random()
+  }
+}
